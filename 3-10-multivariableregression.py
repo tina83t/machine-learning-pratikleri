0e21be349c710488423f7ec327335d69b00c5810
Tina Abiri <tinaabiri@192.168.1.4>
2025-10-05 14:28:58 +0300

    durum calismasi

--- 3-10-multivariableregression.py
+++ 3-10-multivariableregression.py
@@ -25,6 +25,28 @@ y_pred = lin_reg.predict(np.array([x1.flatten(), x2.flatten()]).T)
 ax.plot_surface(x1, x2 , y_pred.reshape(x1. shape))
 plt.title("multi variable linear regression")
 
+print("katsayilar: ",lin_reg.coef_)
+print("kesim:",lin_reg.intercept_)
 
 
+#%%
 
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+diabetes = load_diabetes()
+
+X = diabetes.data 
+y = diabetes.target
+
+X_train ,X_test ,y_train , y_test = train_test_split(X , y ,test_size=0.2 , random_state=42)
+
+lin_reg = LinearRegression()
+lin_reg.fit(X_train, y_train)
+
+y_pred = lin_reg.predict(X_test)
+
+rmse = mean_squared_error (y_test, y_pred, squared=False)
+print("rmse:", rmse)
